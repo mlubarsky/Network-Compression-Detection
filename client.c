3a7b350a6cad16fed66943aaf9172954aa0066a2
max <max@Server.myguest.virtualbox.org>
2024-03-20 17:50:23 -0700

    will send a config file

--- client.c
+++ client.c
@@ -9,50 +9,35 @@
 #define BUFFER_SIZE 1024
 
 int main(int argc, char** argv) {
-    int sock = 0;
-    int valread;
-    
-    struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
-    char message[BUFFER_SIZE];
+    int tcp_sock = 0;
+    struct sockaddr_in serv_addr_tcp;
+    //char buffer[BUFFER_SIZE] = {0};
+    char *config_file = argv[1];
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
+    if ((tcp_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("TCP socket creation error");
         exit(EXIT_FAILURE);
     }
-    
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(SERVER_PORT); // Set server destination port
+	memset(&serv_addr_tcp, 0, sizeof(serv_addr_tcp));
+    serv_addr_tcp.sin_family = AF_INET;
+    serv_addr_tcp.sin_port = htons(SERVER_PORT); // Set server destination port
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr_tcp.sin_addr) <= 0) {
+        perror("Invalid TCP address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
+    if (connect(tcp_sock, (struct sockaddr *)&serv_addr_tcp, sizeof(serv_addr_tcp)) < 0) {
+        perror("TCP Connection Failed");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        printf("Enter message to send to server (type 'quit' to exit): ");
-        fgets(message, BUFFER_SIZE, stdin);
+    // Send message to server
+    send(tcp_sock, config_file, strlen(config_file), 0);
+    printf("Message sent to server\n");
 
-        // Check if user wants to quit
-        if (strncmp(message, "quit", 4) == 0)
-            break;
-
-        // Send message to server
-        send(sock, message, strlen(message), 0);
-        printf("Message sent to server\n");
-
-        // Read response from server
-        valread = read(sock, buffer, BUFFER_SIZE);
-        printf("Server response: %s\n", buffer);
-    }
-
-    close(sock);
+    close(tcp_sock);
     return 0;
 }
