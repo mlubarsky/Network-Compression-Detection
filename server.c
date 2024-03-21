f91b96507bcffc31142a25c2308b82c42ef0f03d
max <max@Server.myguest.virtualbox.org>
2024-03-20 17:56:32 -0700

    config file receieve

--- server.c
+++ server.c
@@ -10,64 +10,49 @@
 int main(int argc, char** argv) {
     int server_fd;
     int new_socket;
-    int valread;
     
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
-    char *message = "Message received";
 
     // Create socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+        perror("TCP socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set socket to port 8765
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+
+    memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(SERVER_PORT);
 
     // Attach socket to  port 8765
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        perror("TCP bind failed");
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("TCP listen failed");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        	perror("accept");
-        	exit(EXIT_FAILURE);
-        }
-
-        while ((valread = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-        	printf("Message from client: %s\n", buffer);
-
-        	// Send response to client
-        	send(new_socket, message, strlen(message), 0);
-        	printf("Response sent to client\n");
-
-        	// Clear buffer
-        	memset(buffer, 0, BUFFER_SIZE);
-        }
-
-        if (valread == 0) {
-        	printf("Client disconnected\n");
-        	close(new_socket);
-        	break;
-        }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+    	perror("TCP accept failed");
+    	exit(EXIT_FAILURE);
     }
 
+	int bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0);
+	if (bytes_received < 0) {
+		perror("TCP receieve failed");
+		exit(EXIT_FAILURE);
+	}
+
+  	buffer[bytes_received] = '\0';
+  	printf("Received configuration file contents from client: %s\n", buffer);
+
+  	close(new_socket);
     close(server_fd);
     return 0;
 }
