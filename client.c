f63dcb5028cdb478a9099c85c44aa7cec0077f6b
Maxwell Lubarsk <mllubarsky@dons.usfca.edu>
2024-03-24 01:57:38 -0700

    client sends config file to server

--- client.c
+++ client.c
@@ -4,40 +4,64 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "169.254.47.121"
+#define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8765
-#define BUFFER_SIZE 1024
 
-int main(int argc, char** argv) {
-    int tcp_sock = 0;
-    struct sockaddr_in serv_addr_tcp;
-    //char buffer[BUFFER_SIZE] = {0};
+void send_config(int tcp_sock, const char *config_file) {
+    FILE *file = fopen(config_file, "r");
+    if (!file) {
+        perror("Failed to open config file");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[1024];
+    ssize_t bytes_read;
+
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        if (send(tcp_sock, buffer, bytes_read, 0) < 0) {
+            perror("Failed to send config file");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Failed to read config file");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(file);
+    printf("Config file sent to server\n");
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <config_file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
     char *config_file = argv[1];
 
-    if ((tcp_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    int tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_sock < 0) {
         perror("TCP socket creation error");
         exit(EXIT_FAILURE);
     }
 
-	memset(&serv_addr_tcp, 0, sizeof(serv_addr_tcp));
-    serv_addr_tcp.sin_family = AF_INET;
-    serv_addr_tcp.sin_port = htons(SERVER_PORT); // Set server destination port
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr_tcp.sin_addr) <= 0) {
-        perror("Invalid TCP address/ Address not supported");
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid TCP address");
         exit(EXIT_FAILURE);
     }
 
-    if (connect(tcp_sock, (struct sockaddr *)&serv_addr_tcp, sizeof(serv_addr_tcp)) < 0) {
+    if (connect(tcp_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("TCP Connection Failed");
         exit(EXIT_FAILURE);
     }
 
-    // Send message to server
-    send(tcp_sock, config_file, strlen(config_file), 0);
-    printf("Message sent to server\n");
-
+    send_config(tcp_sock, config_file);
     close(tcp_sock);
     return 0;
 }
