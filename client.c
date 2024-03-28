6a756cfdba95320a142101c48136032fa8b9e99a
Maxwell Lubarsk <mllubarsky@dons.usfca.edu>
2024-03-28 01:37:36 -0700

    doing something with UDP

--- client.c
+++ client.c
@@ -6,6 +6,8 @@
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8765
+#define UDP_PAYLOAD_SIZE 1000
+#define NUM_UDP_PACKETS 6000
 
 void send_config(int tcp_sock, const char *config_file) {
     FILE *file = fopen(config_file, "r");
@@ -31,6 +33,32 @@ void send_config(int tcp_sock, const char *config_file) {
 
     fclose(file);
     printf("Config file sent to server\n");
+}
+
+void send_udp_packets(int udp_sock) {
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Send low entropy UDP packets
+    for (int i = 0; i < NUM_UDP_PACKETS; i++) {
+        char payload[UDP_PAYLOAD_SIZE];
+        memset(payload, 0, sizeof(payload)); // Fill payload with zeros
+        sendto(udp_sock, payload, sizeof(payload), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    }
+
+    // Wait for Inter-Measurement Time
+
+    sleep(15); // Wait for 15 seconds
+
+    // Send high entropy UDP packets
+    for (int i = 0; i < NUM_UDP_PACKETS; i++) {
+        char payload[UDP_PAYLOAD_SIZE];
+        // Generate random payload here (not implemented in this example)
+        sendto(udp_sock, payload, sizeof(payload), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    }
 }
 
 int main(int argc, char **argv) {
@@ -63,5 +91,15 @@ int main(int argc, char **argv) {
 
     send_config(tcp_sock, config_file);
     close(tcp_sock);
+
+    // Create UDP socket
+    int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_sock < 0) {
+        perror("UDP socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    send_udp_packets(udp_sock);
+    close(udp_sock);
     return 0;
 }
