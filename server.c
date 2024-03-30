6ab78bb5e7320535910976bb810f8f5ee7330220
Maxwell Lubarsk <mllubarsky@dons.usfca.edu>
2024-03-30 03:38:34 -0700

    see comment for client.c

--- server.c
+++ server.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <json-c/json.h>
+#include <time.h>
 
 #define BUFFER_SIZE 1024
+#define THRESHOLD 100
 
-// Store config file contents
-struct Config {
-    char Server_IP_Address[16];
-    int UDP_Source_Port;
-    int UDP_Destination_Port;
-    int TCP_Head_SYN_Port;
-    int TCP_Tail_SYN_Port;
-    int TCP_Pre_Probing_Phase_Port;
-    int TCP_Post_Probing_Phase_Port;
-    int UDP_Payload_Size;
-    int Inter_Measurement_Time;
-    int Number_of_UDP_Packets;
-    int TTL_for_UDP_Packets;
+struct config {
+    char server_ip_address[16];
+    int udp_source_port;
+    int udp_destination_port;
+    int tcp_head_syn_port;
+    int tcp_tail_syn_port;
+    int tcp_pre_probing_phase_port;
+    int tcp_post_probing_phase_port;
+    int udp_payload_size;
+    int inter_measurement_time;
+    int number_of_udp_packets;
+    int ttl_for_udp_packets;
 };
 
-void parse_config(const char *config_json, struct Config *config) {
+void parse_config(const char *config_json, struct config *config) {
     struct json_object *config_obj = json_tokener_parse(config_json);
     if (!config_obj) {
         perror("Failed to parse JSON config");
@@ -35,17 +31,17 @@ void parse_config(const char *config_json, struct Config *config) {
     }
 
     // Extract values from JSON object and store in config struct
-    strcpy(config->Server_IP_Address, json_object_get_string(json_object_object_get(config_obj, "Server_IP_Address")));
-    config->UDP_Source_Port = json_object_get_int(json_object_object_get(config_obj, "UDP_Source_Port"));
-    config->UDP_Destination_Port = json_object_get_int(json_object_object_get(config_obj, "UDP_Destination_Port"));
-    config->TCP_Head_SYN_Port = json_object_get_int(json_object_object_get(config_obj, "TCP_Head_SYN_Port"));
-    config->TCP_Tail_SYN_Port = json_object_get_int(json_object_object_get(config_obj, "TCP_Tail_SYN_Port"));
-    config->TCP_Pre_Probing_Phase_Port = json_object_get_int(json_object_object_get(config_obj, "TCP_Pre_Probing_Phase_Port"));
-    config->TCP_Post_Probing_Phase_Port = json_object_get_int(json_object_object_get(config_obj, "TCP_Post_Probing_Phase_Port"));
-    config->UDP_Payload_Size = json_object_get_int(json_object_object_get(config_obj, "UDP_Payload_Size"));
-    config->Inter_Measurement_Time = json_object_get_int(json_object_object_get(config_obj, "Inter_Measurement_Time"));
-    config->Number_of_UDP_Packets = json_object_get_int(json_object_object_get(config_obj, "Number_of_UDP_Packets"));
-    config->TTL_for_UDP_Packets = json_object_get_int(json_object_object_get(config_obj, "TTL_for_UDP_Packets"));
+    strcpy(config->server_ip_address, json_object_get_string(json_object_object_get(config_obj, "Server_IP_Address")));
+    config->udp_source_port = json_object_get_int(json_object_object_get(config_obj, "UDP_Source_Port"));
+    config->udp_destination_port = json_object_get_int(json_object_object_get(config_obj, "UDP_Destination_Port"));
+    config->tcp_head_syn_port = json_object_get_int(json_object_object_get(config_obj, "TCP_Head_SYN_Port"));
+    config->tcp_tail_syn_port = json_object_get_int(json_object_object_get(config_obj, "TCP_Tail_SYN_Port"));
+    config->tcp_pre_probing_phase_port = json_object_get_int(json_object_object_get(config_obj, "TCP_Pre_Probing_Phase_Port"));
+    config->tcp_post_probing_phase_port = json_object_get_int(json_object_object_get(config_obj, "TCP_Post_Probing_Phase_Port"));
+    config->udp_payload_size = json_object_get_int(json_object_object_get(config_obj, "UDP_Payload_Size"));
+    config->inter_measurement_time = json_object_get_int(json_object_object_get(config_obj, "Inter_Measurement_Time"));
+    config->number_of_udp_packets = json_object_get_int(json_object_object_get(config_obj, "Number_of_UDP_Packets"));
+    config->ttl_for_udp_packets = json_object_get_int(json_object_object_get(config_obj, "TTL_for_UDP_Packets"));
 
     json_object_put(config_obj);
 }
@@ -59,30 +55,64 @@ void receive_config(int client_socket, char *config_buffer) {
     config_buffer[bytes_received] = '\0';
 }
 
-void receive_udp_packets(int udp_sock, struct Config *config) {
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    char payload[config->UDP_Payload_Size];
-    int packets_received = 0;
+void receive_udp_packets(int udp_sock, struct config *config) {
+	struct sockaddr_in client_address;
+	socklen_t len = sizeof(client_address);
+	int UDPbuffer[(config->udp_payload_size)+2];
+	clock_t start_time, end_time;
+	double total_time, low_entropy_time, high_entropy_time;
+	int i, packet_id;
 
-    // Receive UDP packets
-    while (packets_received < config->Number_of_UDP_Packets * 2) {
-        ssize_t bytes_received = recvfrom(udp_sock, payload, sizeof(payload), 0, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (bytes_received < 0) {
-            perror("UDP receive failed");
-            exit(EXIT_FAILURE);
-        }
-        //printf("Received UDP packet %d\n", packets_received + 1);
-        packets_received++;
+	//Receive low entropy data
+    printf("Receiving low entropy \n");
+    memset(UDPbuffer, 0, sizeof(UDPbuffer));
+    start_time = clock();
+    for (i = 0; i < config->number_of_udp_packets; i++) {
+        recvfrom(udp_sock, UDPbuffer, config->udp_payload_size + 2, 0, (struct sockaddr *) &client_address, &len);
+        packet_id = (int)(((unsigned)UDPbuffer[0]) | ((unsigned)UDPbuffer[1] << 8));
+        //packet_id = ntohs(*(uint16_t*)UDPbuffer);
+        printf("Retrieved Low Entropy Packet Number: %d\n", packet_id);
+    }
+    end_time = clock();
+    total_time  = (((double)end_time) - ((double)start_time)) / ((double)CLOCKS_PER_SEC);
+    low_entropy_time = total_time * 1000;
+    printf("Low Entropy Time: %f\n", low_entropy_time);
+
+
+	start_time = 0, end_time = 0;
+
+
+	//Receive high entropy data
+	printf("Receiving high entropy..\n");
+	memset(UDPbuffer, 0, sizeof(UDPbuffer));
+	start_time = clock();
+	memset(&UDPbuffer, 0, config->udp_payload_size + 2);
+	for (i = 0; i < config->number_of_udp_packets; i++) {
+	    recvfrom(udp_sock, UDPbuffer, config->udp_payload_size + 2, 0, (struct sockaddr *) &client_address, &len);
+	    packet_id = ntohs(*(uint16_t*)UDPbuffer);
+	    printf("Retrieved High Entropy Packet Number: %d\n", packet_id);
+	}
+    end_time = clock();
+    total_time  = (((double)end_time) - ((double)start_time)) / ((double)CLOCKS_PER_SEC);
+    high_entropy_time = total_time * 1000;
+    printf("High Entropy Time: %f\n", high_entropy_time);
+
+
+    //Calculate compression
+    if ((high_entropy_time - low_entropy_time) > THRESHOLD) {
+        printf("Compression detected!\n");
+    } else {
+        printf("No compression detected!\n");
     }
 }
 
 int main(int argc, char** argv) {
-	if (argc < 2) {
-		printf("Usage: %s <Port>\n", argv[0]);
-		return -1;
-	}
-    struct Config config;
+    if (argc < 2) {
+	    printf("Usage: %s <Port>\n", argv[0]);
+	    return -1;
+    }
+    
+    struct config config;
     char config_buffer[BUFFER_SIZE];
 
     // Create TCP socket
@@ -123,18 +153,6 @@ int main(int argc, char** argv) {
     receive_config(client_socket, config_buffer);
     parse_config(config_buffer, &config);
 
-    // printf("Server IP Address: %s\n", config.Server_IP_Address);
-    // printf("UDP Source Port: %d\n", config.UDP_Source_Port);
-    // printf("UDP Destination Port: %d\n", config.UDP_Destination_Port);
-    // printf("TCP Head SYN Port: %d\n", config.TCP_Head_SYN_Port);
-    // printf("TCP Tail SYN Port: %d\n", config.TCP_Tail_SYN_Port);
-    // printf("TCP Pre-Probing Phase Port: %d\n", config.TCP_Pre_Probing_Phase_Port);
-    // printf("TCP Post-Probing Phase Port: %d\n", config.TCP_Post_Probing_Phase_Port);
-    // printf("UDP Payload Size: %d\n", config.UDP_Payload_Size);
-    // printf("Inter-Measurement Time: %d\n", config.Inter_Measurement_Time);
-    // printf("Number of UDP Packets: %d\n", config.Number_of_UDP_Packets);
-    // printf("TTL for UDP Packets: %d\n", config.TTL_for_UDP_Packets);
-
     // Create UDP socket
     int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (udp_sock < 0) {
@@ -145,7 +163,7 @@ int main(int argc, char** argv) {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(config.UDP_Destination_Port);
+    server_addr.sin_port = htons(config.udp_destination_port);
 
     // Bind socket to port
     if (bind(udp_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -155,7 +173,6 @@ int main(int argc, char** argv) {
 
     receive_udp_packets(udp_sock, &config);
 
-    // Close sockets
     close(client_socket);
     close(server_fd);
     close(udp_sock);
