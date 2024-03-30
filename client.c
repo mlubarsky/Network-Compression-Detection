8f7fa9758a7b69c89a9a3865dc919582d52bacf1
Maxwell Lubarsk <mllubarsky@dons.usfca.edu>
2024-03-30 15:24:27 -0700

    updated ports

--- client.c
+++ client.c
@@ -21,13 +21,6 @@ struct config {
     int ttl_for_udp_packets;
 };
 
-void set_packet_id(int *data, int index) {
-    unsigned char lsb = (unsigned)index & 0xff; // LSB
-    unsigned char msb = (unsigned)index >> 8;   // MSB
-    data[0] = lsb; // set the first index to lower bit
-    data[1] = msb; // set the second index to upper bit
-}
-
 void parse_config(const char *config_json, struct config *config) {
     struct json_object *config_obj = json_tokener_parse(config_json);
     if (!config_obj) {
@@ -60,7 +53,21 @@ void read_config_file(const char *config_file, char *config_buffer) {
     fclose(file);
 }
 
-void send_config(int tcp_sock, const char *config_file) {
+void send_tcp_packets(int tcp_sock, struct config *config, char* config_file) {
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(config->tcp_pre_probing_phase_port);
+    if (inet_pton(AF_INET, config->server_ip_address, &server_addr.sin_addr) <= 0) {
+        perror("Invalid TCP address");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(tcp_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("TCP Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
     FILE *file = fopen(config_file, "r");
     if (!file) {
         perror("Failed to open config file");
@@ -86,34 +93,23 @@ void send_config(int tcp_sock, const char *config_file) {
     printf("Config file sent to server\n");
 }
 
-/*
-	Pre-probing Phase
-
-	Sends the config file contents to the server
-*/
-void send_tcp_packets(int tcp_sock, struct config *config, char* config_file) {
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(config->udp_destination_port);
-
-    if (inet_pton(AF_INET, config->server_ip_address, &server_addr.sin_addr) <= 0) {
-        perror("Invalid TCP address");
+// Function to receive compression detection message from the server via TCP
+void receive_detection_message(int tcp_sock) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received = recv(tcp_sock, buffer, BUFFER_SIZE, 0);
+    if (bytes_received < 0) {
+        perror("TCP receive failed");
         exit(EXIT_FAILURE);
     }
-
-    if (connect(tcp_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("TCP Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-
-    send_config(tcp_sock, config_file);
+    buffer[bytes_received] = '\0';
+    printf("Compression Detection Message from Server: %s\n", buffer);
 }
 
-/*
-	Probing Phase
 
-	Creates low and high entropy packet trains and sends them to the server over UDP
+/*
+    Probing Phase
+
+    Creates low and high entropy packet trains and sends them to the server over UDP
 */
 void send_udp_packets(int udp_sock, struct config *config) {
     char high_entropy[config->udp_payload_size];
@@ -131,28 +127,21 @@ void send_udp_packets(int udp_sock, struct config *config) {
     server_addr.sin_port = htons(config->udp_destination_port);
     server_addr.sin_addr.s_addr = inet_addr(config->server_ip_address);
 
-    if (connect(udp_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("UDP Connection Failed");
-        exit(EXIT_FAILURE);
-    }
-
     // Set the df flag in the IP header
     int DF = IP_PMTUDISC_DO;
-    if (setsockopt(udp_sock, IPPROTO_IP, IP_MTU_DISCOVER, &DF, sizeof(DF)) < 0) {;
+    if (setsockopt(udp_sock, IPPROTO_IP, IP_MTU_DISCOVER, &DF, sizeof(DF)) < 0) {
         perror("Failed to set DF flag");
         exit(EXIT_FAILURE);
     }
 
     //Send low entropy UDP packets
     for (int i = 0; i < config->number_of_udp_packets; i++) {
-        // char payload[config->udp_payload_size + 2]; // Include space for packet ID
-        // set_packet_id((int*)(payload), i);
-
         char payload[config->udp_payload_size];
         *(uint16_t*)payload = htons(i);
-        
+
         memset(payload + 2, 0, config->udp_payload_size - 2); // Fill remaining payload with zeros
         sendto(udp_sock, payload, (config->udp_payload_size) + 2, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        usleep(200);
     }
     printf("Sent low entropy packets\n");
 
@@ -165,23 +154,11 @@ void send_udp_packets(int udp_sock, struct config *config) {
 
         memcpy(payload + 2, high_entropy, config->udp_payload_size - 2); // Copy high entropy data to payload
         sendto(udp_sock, payload, (config->udp_payload_size) + 2, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        usleep(200);
     }
     printf("Sent high entropy packets\n");
-
-    //sleep(5); // Server catch up
 }
 
-// Function to receive compression detection message from the server via TCP
-void receive_detection_message(int tcp_sock) {
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_received = recv(tcp_sock, buffer, BUFFER_SIZE, 0);
-    if (bytes_received < 0) {
-        perror("TCP receive failed");
-        exit(EXIT_FAILURE);
-    }
-    buffer[bytes_received] = '\0';
-    printf("Compression Detection Message from Server: %s\n", buffer);
-}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -216,7 +193,7 @@ int main(int argc, char **argv) {
 
     send_tcp_packets(tcp_sock, &config, config_file);
     send_udp_packets(udp_sock, &config);
-    
+
     receive_detection_message(tcp_sock);
 
     close(tcp_sock);
