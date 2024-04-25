f622832e8eecf3dd493ae847b58452a7bd8de215
max <max@Server.myguest.virtualbox.org>
2024-04-24 19:05:01 -0700

    multithreading working

--- standalone.c
+++ standalone.c
@@ -8,11 +8,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
 #define PACKET_LEN 4096
-#define SRC_PORT_X 1234         // Source port X
-#define SRC_PORT_Y 1235         // Source port Y
+#define SRC_PORT_X 1234     	// Source port X
+#define SRC_PORT_Y 1235     	// Source port Y
 
 struct config {
     char server_ip_address[16];
@@ -104,12 +105,211 @@ unsigned short ip_checksum(struct iphdr *iph) {
     return (unsigned short)(~sum);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        printf("Usage: %s <config_file>\n", argv[0]);
+void send_udp_packets(int udp_sock, struct config *config) {
+    char high_entropy[config->udp_payload_size];
+    FILE *urandom = fopen("/dev/urandom", "r");
+    if (!urandom) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+    fread(high_entropy, 1, config->udp_payload_size, urandom);
+    fclose(urandom);
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(config->udp_destination_port);
+    server_addr.sin_addr.s_addr = inet_addr(config->server_ip_address);
+
+    struct sockaddr_in client_addr;
+	memset(&client_addr, 0, sizeof(client_addr));
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	client_addr.sin_port = htons(config->udp_source_port);
+
+	int reuseaddr = 1;
+	if (setsockopt(udp_sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr)) < 0) {
+		perror("Invalid address");
+		exit(EXIT_FAILURE);
+	}
+
+	if (bind(udp_sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
+		perror("Bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+    // Set the df flag in the IP header
+    int DF = IP_PMTUDISC_DO;
+    if (setsockopt(udp_sock, IPPROTO_IP, IP_MTU_DISCOVER, &DF, sizeof(DF)) < 0) {
+        perror("Failed to set DF flag");
         exit(EXIT_FAILURE);
     }
 
+    //Send low entropy UDP packets
+    for (int i = 0; i < config->number_of_udp_packets; i++) {
+        char payload[config->udp_payload_size]; // Set payload size to be 1000
+        *(uint16_t*)payload = htons(i);
+
+        memset(payload + 2, 0, config->udp_payload_size - 2); // Fill payload with zeros
+        sendto(udp_sock, payload, (config->udp_payload_size), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        usleep(200);
+    }
+    printf("Low entropy UDP packets sent\n");
+
+	printf("Sleeping for %d seconds\n", config->inter_measurement_time);
+    sleep(config->inter_measurement_time); // Wait for 15 seconds
+
+    //Send high entropy UDP packets
+    for (int i = 0; i < config->number_of_udp_packets; i++) {
+        char payload[config->udp_payload_size]; // Set payload size to be 1000
+        *(uint16_t*)payload = htons(i);
+
+        memcpy(payload + 2, high_entropy, config->udp_payload_size - 2); // Copy high entropy data to payload
+        sendto(udp_sock, payload, (config->udp_payload_size), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        usleep(200);
+    }
+    printf("High entropy UDP packets sent\n");
+}
+
+void *send_packets(void *arg) {
+    struct config *config = (struct config *)arg;
+
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Fill in IP header and TCP header for SYN head packet
+    struct sockaddr_in dest_addr;
+    char packet[PACKET_LEN];
+    struct iphdr *ip_header = (struct iphdr *)packet;
+    struct tcphdr *tcp_header = (struct tcphdr *)(packet + sizeof(struct iphdr));
+    
+    ip_header->ihl = 5;
+    ip_header->version = 4;
+    ip_header->tos = 0;
+    ip_header->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr));
+    ip_header->id = 0;
+    ip_header->frag_off = 0;
+    ip_header->ttl = 255;
+    ip_header->protocol = IPPROTO_TCP;
+    ip_header->check = 0;
+    ip_header->saddr = inet_addr("169.254.200.14");
+    ip_header->daddr = inet_addr(config->server_ip_address);
+    
+    tcp_header->source = htons(SRC_PORT_X);
+    tcp_header->dest = htons(config->tcp_head_syn_port);
+    tcp_header->seq = 0;
+    tcp_header->ack_seq = 0;
+    tcp_header->doff = 5;
+    tcp_header->fin = 0;
+    tcp_header->syn = 1;
+    tcp_header->rst = 0;
+    tcp_header->psh = 0;
+    tcp_header->ack = 0;
+    tcp_header->urg = 0;
+    tcp_header->window = htons(5840);
+    tcp_header->check = 0;
+    tcp_header->urg_ptr = 0;
+
+    // Calculate TCP checksum
+    tcp_header->check = tcp_checksum(ip_header, tcp_header);
+
+    // Fill in destination address structure
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_addr.s_addr = ip_header->daddr;
+
+    // Send SYN head packet
+    if (sendto(sockfd, packet, ntohs(ip_header->tot_len), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+        perror("sendto");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up UDP socket
+    int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_sock < 0) {
+        perror("UDP socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send UDP packets
+    sleep(1); // Sleep to catch the RST first before sending UDP packets
+    send_udp_packets(udp_sock, config);
+
+    // Fill in the IP header and TCP header for SYN tail packet
+    ip_header->saddr = inet_addr("169.254.200.14");
+    ip_header->daddr = inet_addr(config->server_ip_address);
+    tcp_header->source = htons(SRC_PORT_Y);
+    tcp_header->dest = htons(config->tcp_tail_syn_port);
+
+    tcp_header->check = 0;
+    tcp_header->check = tcp_checksum(ip_header, tcp_header);
+
+    // Send SYN tail packet
+    if (sendto(sockfd, packet, ntohs(ip_header->tot_len), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+        perror("sendto");
+        exit(EXIT_FAILURE);
+    }
+
+    close(sockfd);
+    close(udp_sock);
+
+    return NULL;
+}
+
+void *receive_rst_packets(void *arg) {
+    int recvsock;
+    if ((recvsock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive RST for head packet
+    char recv_buffer[PACKET_LEN];
+    int recv_len;
+    while (1) {
+        recv_len = recvfrom(recvsock, recv_buffer, PACKET_LEN, 0, NULL, NULL);
+        struct iphdr *recv_ip_header = (struct iphdr *)recv_buffer;
+        struct tcphdr *recv_tcp_header = (struct tcphdr *)(recv_buffer + sizeof(struct iphdr));
+        if (recv_ip_header->protocol == IPPROTO_TCP && recv_tcp_header->rst) {
+            printf("SYN head RST packet received.\n");
+            break;
+        }
+    }
+    if (recv_len < 0) {
+        perror("recvfrom");
+        exit(EXIT_FAILURE);
+    }
+    
+	// Receive RST for tail packet
+    while (1) {
+		recv_len = recvfrom(recvsock, recv_buffer, PACKET_LEN, 0, NULL, NULL);
+		struct iphdr *recv_ip_header = (struct iphdr *)recv_buffer;
+		struct tcphdr *recv_tcp_header = (struct tcphdr *)(recv_buffer + sizeof(struct iphdr));
+		if (recv_ip_header->protocol == IPPROTO_TCP && recv_tcp_header->rst) {
+			printf("SYN tail RST packet received.\n");
+			break;
+		}
+	}
+	if (recv_len < 0) {
+		perror("recvfrom");
+		exit(EXIT_FAILURE);
+	}
+
+    close(recvsock);
+    
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+	    printf("Usage: %s <config_file>\n", argv[0]);
+	    exit(EXIT_FAILURE);
+	}
+
+    pthread_t send_thread, receive_thread;
     struct config config;
     char config_buffer[BUFFER_SIZE];
     char *config_file = argv[1];
@@ -121,82 +321,20 @@ int main(int argc, char **argv) {
     read_config_file(config_file, config_buffer);
     parse_config(config_buffer, &config);
 
-
-    struct sockaddr_in dest_addr;
-    char packet[PACKET_LEN];
-
-    // Fill in the IP header
-    struct iphdr *ip_header = (struct iphdr *) packet;
-    ip_header->ihl = 5;
-    ip_header->version = 4;
-    ip_header->tos = 0;
-    ip_header->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr));
-    // ip_header->id = htons(54321);
-    ip_header->id = 0;
-    ip_header->frag_off = 0;
-    ip_header->ttl = 255;
-    ip_header->protocol = IPPROTO_TCP;
-    ip_header->check = 0;  // Checksum to be filled later
-    ip_header->saddr = inet_addr("169.254.200.14");
-    ip_header->daddr = inet_addr(config.server_ip_address);
-
-    // Fill in the TCP header
-    struct tcphdr *tcp_header = (struct tcphdr *) (packet + sizeof(struct iphdr));
-    tcp_header->source = htons(SRC_PORT_X); // Source port X
-    tcp_header->dest = htons(config.tcp_head_syn_port);  // Destination port for SYN head
-    tcp_header->seq = 0;
-    tcp_header->ack_seq = 0;
-    tcp_header->doff = 5;
-    tcp_header->fin = 0;
-    tcp_header->syn = 1;  // TCP SYN flag
-    tcp_header->rst = 0;
-    tcp_header->psh = 0;
-    tcp_header->ack = 0;
-    tcp_header->urg = 0;
-    tcp_header->window = htons(5840);  // Maximum allowed window size
-    tcp_header->check = 0;  // Checksum to be filled later
-    tcp_header->urg_ptr = 0;
-
-    // Calculate TCP checksum
-    tcp_header->check = tcp_checksum(ip_header, tcp_header);
-
-    // Fill in destination address structure
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_addr.s_addr = ip_header->daddr;
-
-	// Setup socket and send SYN packet
-    int sockfd;
-    if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-    if (sendto(sockfd, packet, ntohs(ip_header->tot_len), 0, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0) {
-        perror("sendto");
+    // Create send_packets thread
+    if (pthread_create(&send_thread, NULL, send_packets, &config) != 0) {
+        perror("pthread_create for send_packets");
         exit(EXIT_FAILURE);
     }
 
-
-    // Receive RST packets
-    // struct sockaddr_in recv_addr;
-    // socklen_t addr_len = sizeof(recv_addr);
-    char recv_buffer[PACKET_LEN];
-    int recv_len;
-    while ((recv_len = recvfrom(sockfd, recv_buffer, PACKET_LEN, 0, NULL, NULL)) > 0) {
-        struct iphdr *recv_ip_header = (struct iphdr *) recv_buffer;
-        struct tcphdr *recv_tcp_header = (struct tcphdr *) (recv_buffer + sizeof(struct iphdr));
-        if (recv_ip_header->protocol == IPPROTO_TCP && recv_tcp_header->rst) {
-            printf("RST packet received.\n");
-            break;
-        }
-    }
-    if (recv_len < 0) {
-        perror("recvfrom");
+    // Create receive_rst_packets thread
+    if (pthread_create(&receive_thread, NULL, receive_rst_packets, NULL) != 0) {
+        perror("pthread_create for receive_rst_packets");
         exit(EXIT_FAILURE);
     }
-    
 
-    close(sockfd);
-
+    pthread_join(send_thread, NULL);
+    pthread_join(receive_thread, NULL);
+	
     return 0;
 }
